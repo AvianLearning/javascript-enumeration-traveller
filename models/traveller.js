a0393731b4732d054b1ded5711a344e81a1c58dd
AvianLearning <iaradcliffe@hotmail.com>
2020-10-14 20:14:13 +0100

    Passed get journeys by transport

--- models/traveller.js
+++ models/traveller.js
@@ -12,15 +12,18 @@ Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
     return journey.endLocation;
   })
-
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.every((journey) => {
+    return journey.distance > minDistance; 
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
